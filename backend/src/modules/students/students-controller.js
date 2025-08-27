285f88caf6f956630518a12036b52ee36c27e1fc
ER8IK <locha.eth@gmail.com>
2025-08-27 19:54:58 +0400

    first commit

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -1,29 +1,57 @@
 const asyncHandler = require("express-async-handler");
-const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent } = require("./students-service");
+const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updateStudent, deleteStudent } = require("./students-service");
+const { ApiError } = require("../../utils");
 
+// get all students
 const handleGetAllStudents = asyncHandler(async (req, res) => {
-    //write your code
-
+    const {page, limit, name, class: className, section, roll} = req.query;
+    const students = await getAllStudents({ 
+        page: page ? parseInt(page, 10) : 1, 
+        limit: limit ? parseInt(limit, 10) : 10, 
+        name, 
+        className, 
+        section, 
+        roll: roll ? parseInt(roll) : undefined
+    });
+    res.json(students);
 });
 
+// post student
 const handleAddStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+    const payload = req.body;
+    const message = await addNewStudent(payload);
+    res.json(message);
 });
 
+// put student :id
 const handleUpdateStudent = asyncHandler(async (req, res) => {
-    //write your code
-
+   const {id} = {...req.params};
+   const payload = {...req.body, userId: id};
+   try {
+   await updateStudent(payload);
+    res.status(200).json({ message: "Student updated successfully." });
+   } catch (error) {
+    throw new ApiError(500, "Unable to update student");
+   } 
 });
 
+// get student :id
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
-    //write your code
-
+    const student = await getStudentDetail(req.params.id);
+    res.json(student);
 });
 
+// post student :id/status
 const handleStudentStatus = asyncHandler(async (req, res) => {
-    //write your code
+    const payload = {...req.body, userId: req.params.id};
+    const result = await setStudentStatus(payload);
+    res.status(200).json(result);
+});
 
+const handleDeleteStudent = asyncHandler(async (req, res) => {
+    const {id} = req.params;
+    const result = await deleteStudent(id);
+    res.status(200).json(result);
 });
 
 module.exports = {
@@ -32,4 +60,5 @@ module.exports = {
     handleAddStudent,
     handleStudentStatus,
     handleUpdateStudent,
+    handleDeleteStudent
 };
